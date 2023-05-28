27774d70616ec892fe349178424d70c339b37f1b
MJ Cariso <111568194+emjay03@users.noreply.github.com>
2023-05-28 20:35:45 +0800

    home

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,10 +1,44 @@
 import React, { useEffect } from 'react';
 import { useLocation , useNavigate } from "react-router-dom";
+import { Avatar, WrapItem } from '@chakra-ui/react'
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+ 
+  MenuGroup,
+  Button,
+ 
+  MenuDivider,
+} from '@chakra-ui/react'
+import { PhoneIcon, AddIcon, WarningIcon,HamburgerIcon,ChevronRightIcon } from '@chakra-ui/icons'
+
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Radio,
+  useDisclosure,
+  RadioGroup,
+  Stack,
+} from '@chakra-ui/react'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from '@chakra-ui/react'
 function Home() {
   const location = useLocation();
   const sessionName = location.state?.sessionName || ""; // Retrieve sessionName from location state
   const navigate = useNavigate();
-
+  const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true });
+  const [placement, setPlacement] = React.useState('left')
   useEffect(() => {
     // Check if sessionName is not present or empty, then navigate to the login page
     if (!sessionName) {
@@ -13,8 +47,53 @@ function Home() {
   }, [sessionName, navigate]);
 
   return (
-    <div>
-         <h1>Welcome, {sessionName}!</h1>
+    <div className='home-container'>
+       
+        <div className='home-header'>
+        <h1>TO DO</h1>
+
+ 
+         
+      <Button colorScheme='blue' onClick={onOpen}>
+      <HamburgerIcon />
+      </Button>
+      <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+          <DrawerBody>
+          <WrapItem className="WrapItem">
+    <Avatar name='Dan Abrahmov' className="avatar-img"src='https://bit.ly/dan-abramov' />
+    
+    <h1>Welcome,<br></br> {sessionName}!</h1>
+    
+  </WrapItem>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+       
+        
+ 
+      </div>
+      <div className='home-body'>
+      <Breadcrumb spacing='8px' separator={<ChevronRightIcon color='gray.500' />}>
+  <BreadcrumbItem>
+    <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+  </BreadcrumbItem>
+
+  
+  
+</Breadcrumb>
+
+<div className='board'>
+<div className='board-t'>s</div>
+<div className='board-t'>s</div>
+<div className='board-t'>s</div>
+</div>
+     </div>
     </div>
   )
 }
